0daadd91db53bd5fbc80359c4e47bf085ce9d834
Mikołaj Rodak <mrodak03@gmail.com>
2023-09-06 17:10:04 +0200

    koniec

--- main.js
+++ main.js
@@ -46,6 +46,30 @@ const checkPassword = (pass1, pass2) => {
 	}
 }
 
+const checkMail = email => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'E-mail jest niepoprawny')
+	}
+}
+
+const checkError = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) errorCount++
+	})
+
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
+}
+
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
 
@@ -53,6 +77,8 @@ sendBtn.addEventListener('click', e => {
 	checkLenght(username, 3)
 	checkLenght(pass, 8)
 	checkPassword(pass, pass2)
+	checkMail(email)
+	checkError()
 })
 
 // preventDefalut zapobiega przeładowywaniu sie strony
@@ -60,5 +86,6 @@ clearBtn.addEventListener('click', e => {
 	e.preventDefault()
 	;[username, pass, pass2, email].forEach(el => {
 		el.value = ''
+		clearError(el)
 	})
 })
